4f02b6351bd1c46c774c9f240022485846496cf2
UHT <1404119365@qq.com>
2019-11-16 23:05:30 +0800

    V0.01

---
+++ src/main/java/com/UHT/Insight/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.UHT.Insight.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(
+            HttpServletRequest request,
+                        Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "5") Integer size) {
+        return "index";
+    }
+}
