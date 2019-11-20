4e985acafea709a4029cdae70e41a4703c56746c
LinCount <1791566425@qq.com>
2019-11-20 10:47:45 +0800

    上传遗漏2

---
+++ src/main/java/com/UHT/Insight/dao/RecentPlayDao.java
@@ -0,0 +1,18 @@
+package com.UHT.Insight.dao;
+
+import com.UHT.Insight.pojo.RecentPlay;
+
+import java.util.List;
+
+public interface RecentPlayDao {
+    //查询所有
+    List<RecentPlay> findAll();
+    //插入
+    Integer saveRecentPlay(RecentPlay recentPlay);
+    //根据ID查询
+    RecentPlay findRecentPlayById(Integer R_ID);
+    //根据Id修改
+    Integer updateRecentPlay(RecentPlay recentPlay);
+    //根据ID删除
+    Integer deleteRecentPlayById(Integer R_ID);
+}
---
+++ src/test/java/com/UHT/Insight/daoimpl/RecentPlayDaoTest.java
@@ -0,0 +1,36 @@
+package com.UHT.Insight.daoimpl;
+
+import com.UHT.Insight.daoImpl.RecentPlayDaoImpl;
+import com.UHT.Insight.pojo.RecentPlay;
+import org.junit.Test;
+
+public class RecentPlayDaoTest {
+    private RecentPlayDaoImpl recentPlayDao=new RecentPlayDaoImpl();
+    //查询所有
+    @Test
+    public void findAll(){
+        System.out.println(recentPlayDao.findAll().toString());
+    }
+    //插入
+    @Test
+    public void saveRecentPlay(){
+        RecentPlay recentPlay=new RecentPlay(1132,2323,"校内","23122时","飙车");
+        System.out.println(recentPlayDao.saveRecentPlay(recentPlay));
+    }
+    //根据ID查询
+    @Test
+    public void findRecentPlayById(){
+        System.out.println(recentPlayDao.findRecentPlayById(1111).toString());
+    }
+    //根据Id修改
+    @Test
+    public void updateRecentPlay(){
+        RecentPlay recentPlay=new RecentPlay(1132,23233,"校内","23122时","飙车");
+        System.out.println(recentPlayDao.updateRecentPlay(recentPlay));
+    }
+    //根据ID删除
+    @Test
+    public void deleteRecentPlayById(){
+         System.out.println(recentPlayDao.deleteRecentPlayById(1111));
+    }
+}
