c1889f980db5dd8dfd62592353e16c079bcee46d
UHT <1404119365@qq.com>
2019-11-23 11:20:47 +0800

    原神接口完成

---
+++ src/test/java/com/UHT/Insight/controller/GameControllerTest.java
@@ -0,0 +1,12 @@
+package com.UHT.Insight.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameControllerTest {
+
+    @Test
+    void game_main() {
+    }
+}
