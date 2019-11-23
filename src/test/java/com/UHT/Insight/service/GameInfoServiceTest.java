20fd7bfa492297fa895da82d6cab02f996957c3b
UHT <1404119365@qq.com>
2019-11-23 11:28:47 +0800

    原神接口完成

--- src/test/java/com/UHT/Insight/service/GameInfoServiceTest.java
+++ src/test/java/com/UHT/Insight/service/GameInfoServiceTest.java
@@ -14,6 +14,5 @@ class GameInfoServiceTest {
     void getGameDayInfoList() {
         List<GameDayInfo> gameDayInfoList = gameInfoService.getGameDayInfoList(23234);
         System.out.println(gameDayInfoList);
-
     }
 }
