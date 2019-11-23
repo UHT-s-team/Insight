84f11d6adbfee4fb1042652f7c4e7ccaa5c1e135
UHT <1404119365@qq.com>
2019-11-23 11:22:31 +0800

    原神接口完成

---
+++ src/test/java/com/UHT/Insight/service/GameInfoServiceTest.java
@@ -0,0 +1,19 @@
+package com.UHT.Insight.service;
+
+import com.UHT.Insight.dto.GameDayInfo;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameInfoServiceTest {
+
+    GameInfoService gameInfoService=new GameInfoService();
+    @Test
+    void getGameDayInfoList() {
+        List<GameDayInfo> gameDayInfoList = gameInfoService.getGameDayInfoList(23234);
+        System.out.println(gameDayInfoList);
+
+    }
+}
