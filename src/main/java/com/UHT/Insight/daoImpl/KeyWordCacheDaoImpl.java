45703efeb59b8c2af0fc11f2238d3232683db9d6
LinCount <1791566425@qq.com>
2019-12-15 20:35:21 +0800

    keywordcache表的建立

---
+++ src/main/java/com/UHT/Insight/daoImpl/KeyWordCacheDaoImpl.java
@@ -0,0 +1,65 @@
+package com.UHT.Insight.daoImpl;
+
+import com.UHT.Insight.dao.KeyWordCacheDao;
+import com.UHT.Insight.pojo.KeyWordCache;
+import com.UHT.Insight.utils.MybatilsUtils;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+
+public class KeyWordCacheDaoImpl {
+    private SqlSession session= MybatilsUtils.getSession();
+    private KeyWordCacheDao keyWordCacheDao=session.getMapper(KeyWordCacheDao.class);
+    //读取数据
+    public KeyWordCache findKeyWordCacheById(Integer K_ID){
+        KeyWordCache keyWordCache=null;
+        try {
+            keyWordCache=keyWordCacheDao.findKeyWordCacheById(K_ID);
+            MybatilsUtils.destroy();
+        }catch (Exception e){
+            e.printStackTrace();
+            session.rollback();
+            return keyWordCache;
+        }
+        return keyWordCache;
+    }
+    //插入数据
+    public Integer saveKeyWordDataCache(KeyWordCache keyWordCache){
+        Integer i=-1;
+        try {
+            i=keyWordCacheDao.saveKeyWordDataCache(keyWordCache);
+            session.commit();
+        }catch (Exception e){
+            e.printStackTrace();
+            session.rollback();
+            return i;
+        }
+        return i;
+    }
+    //更新数据
+    public Integer updateKeyWordDataCache(KeyWordCache keyWordCache){
+        Integer i=-1;
+        try {
+            i=keyWordCacheDao.updateKeyWordDataCache(keyWordCache);
+            MybatilsUtils.destroy();
+        }catch (Exception e){
+            e.printStackTrace();
+            session.rollback();
+            return i;
+        }
+        return i;
+    }
+    //查找所有的id及创建时间
+    public List<KeyWordCache> findAllKeyWordDataCache(){
+        List<KeyWordCache> list=null;
+        try {
+            list=keyWordCacheDao.findAllKeyWordDataCache();
+            MybatilsUtils.destroy();
+        }catch (Exception e){
+            e.printStackTrace();
+            session.rollback();
+            return list;
+        }
+        return list;
+    }
+}
