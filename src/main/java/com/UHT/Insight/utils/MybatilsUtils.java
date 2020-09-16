1584e4d3306758395b5425e965937a7eb858d356
huangning <huangning@wezhuiyi.com>
2020-09-16 13:21:11 +0800

    稍微改了一下数据库链接，现在应该能用了？？？

--- src/main/java/com/UHT/Insight/utils/MybatilsUtils.java
+++ src/main/java/com/UHT/Insight/utils/MybatilsUtils.java
@@ -1,11 +1,15 @@
 package com.UHT.Insight.utils;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import javax.sql.DataSource;
 import java.io.InputStream;
+import java.util.Properties;
 
 public class MybatilsUtils {
 //    private static SqlSessionFactory sqlSessionFactory=null;
@@ -24,11 +28,22 @@ public class MybatilsUtils {
 //            e.printStackTrace();
 //        }
 //    }
+
+    public DataSource getDataSource(){
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDataSource(null);
+
+        HikariDataSource hikariDataSource = new HikariDataSource(hikariConfig);
+        return hikariDataSource;
+    }
+
     //获取SqlSession对象的静态方法
     public  SqlSession getSession(){
         SqlSessionFactory sqlSessionFactory=null;
         try {
             InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
+            Properties resourceAsProperties = Resources.getResourceAsProperties("SqlMapConfig.xml");
+//            resourceAsProperties.
             //2.创建 SqlSessionFactory 的构建者对象
             SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
             //3.使用构建者创建工厂对象 SqlSessionFactory
@@ -40,5 +55,11 @@ public class MybatilsUtils {
         }
 //        return sqlSession=sqlSessionFactory.openSession();
         return sqlSessionFactory.openSession();
+    }
+
+    public static void main(String[] args) {
+        MybatilsUtils mybatilsUtils = new MybatilsUtils();
+        SqlSession session = mybatilsUtils.getSession();
+//        session.e
     }
 }
